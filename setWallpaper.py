5fd976b7da9fe865af61c8452d8786c06d4b3bba
Aditya Bhosale <140010009@iitb.ac.in>
2015-04-29 00:40:20 +0530

    update wallpaper

--- setWallpaper.py
+++ setWallpaper.py
@@ -1,5 +1,7 @@
 from download import download
 import os
+import time
 
 download()
+time.sleep(2)
 os.system('gsettings set org.gnome.desktop.background picture-uri file://' + str(os.path.realpath('wallpaper.jpg')))
