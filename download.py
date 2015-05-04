c1505bd5911e10bd5366da8e7deec5e4cef5dcf3
Aditya Bhosale <140010009@iitb.ac.in>
2015-05-05 00:56:56 +0530

    remove pixelation bug

--- download.py
+++ download.py
@@ -1,8 +1,9 @@
 from getUrl import getUrl
 import os
 import urllib
+from datetime import date
 
 def download():
 	url = getUrl()
 	print url
-	os.system('wget ' + str(url) + " -O wallpaper.jpg")
+	os.system('wget ' + str(url) + " -O " + str(os.getcwd()) + "/wallpapers/wallpaper" + str(date.today()) + ".jpg")
--- setWallpaper.py
+++ setWallpaper.py
@@ -1,7 +1,6 @@
 from download import download
 import os
-import time
+from datetime import date
 
 download()
-time.sleep(2)
-os.system('gsettings set org.gnome.desktop.background picture-uri file://' + str(os.path.realpath('wallpaper.jpg')))
+os.system('gsettings set org.gnome.desktop.background picture-uri file://' + str(os.getcwd()) + '/wallpapers/wallpaper' + str(date.today()) + '.jpg')
