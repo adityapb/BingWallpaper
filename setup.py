f134a8f5da9c6913a5ab73fa0b96a2709e392783
Aditya Bhosale <140010009@iitb.ac.in>
2015-05-05 00:59:11 +0530

    update setup

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ if X.lower() == 'y':
 	os.system("wget 'https://bootstrap.pypa.io/get-pip.py'")
 	os.system("sudo python get-pip.py")
 	os.system("sudo pip install beautifulsoup4")
+	os.system("mkdir " + os.getcwd() + "/wallpapers")
 	os.system("echo 'To use, cd to the directory with all the code. Use \'python setWallpaper.py\' in terminal to run'")
